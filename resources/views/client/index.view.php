6b655f10344a62c1e29b3f803ee0e53e0885d75f
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-07 13:59:19 +0800

    Add comment

--- resources/views/client/index.view.php
+++ resources/views/client/index.view.php
@@ -1,13 +1,13 @@
 <?php view('includes/header'); ?>
 <!-- Page Content -->
 <div class="h-100 w-100">
-  
+
   <?php view('client/toolbar'); ?>
 
   <div class="container-fluid h-100 mt-4">
 
-    <div class="row">
-      <p class="h3 mx-auto"> Welcome to Information Technology Services Request Page, <?= user()->username ?> !</p>
+    <div class="container">
+      <h1 class="h3 mx-auto"> Welcome to Information Technology Services Request Page, <?= user()->username ?>.</h1>
     </div>
 
     <hr class="border border-bottom border-success">
@@ -17,15 +17,11 @@
         <div class="col-lg-6">
           <div class="row text-center">
             <div class="col-12">
-              <button 
-                class="btn btn-request btn-success" 
-                id="request" 
-                data-toggle="tooltip" 
-                data-placement="left" 
-                title="Request"
-                onclick="$.redirect('<?=getPath('client/add-request.php')?>')">
-                <i class="fa fa-phone-square" aria-hidden="true"></i> 
+              <!-- Send Request Button -->
+              <button class="btn btn-request btn-success" id="request" data-toggle="tooltip" data-placement="left" title="Request" onclick="$.redirect('<?= getPath('client/add-request.php') ?>')">
+                <i class="fa fa-phone-square" aria-hidden="true"></i>
               </button>
+              <!-- /# Send Request Button -->
             </div>
             <div class="col-12">
               <small>Send a request</small>
@@ -35,15 +31,11 @@
         <div class="col-lg-6">
           <div class="row text-center">
             <div class="col-12">
-              <button 
-                class="btn btn-request btn-success" 
-                id="track" 
-                data-toggle="tooltip" 
-                data-placement="right" 
-                title="Track Requests"
-                onclick="$.redirect('<?=getPath('client/sent-requests.php')?>')">
+              <!-- View Sent Requests Button -->
+              <button class="btn btn-request btn-success" id="track" data-toggle="tooltip" data-placement="right" title="Track Requests" onclick="$.redirect('<?= getPath('client/sent-requests.php') ?>')">
                 <i class="fa fa-eye" aria-hidden="true"></i>
               </button>
+              <!-- /# View Sent Requests Button -->
             </div>
             <div class="col-12">
               <small>View Sent Requests</small>
