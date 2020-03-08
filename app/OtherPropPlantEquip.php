e5fdc1450ce60ce9fbec882657cf4f642689c5a6
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-08 12:55:37 +0800

    Create model

---
+++ app/OtherPropPlantEquip.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use App\DB;
+
+class OtherPropPlantEquip
+{
+  /**
+   * Create an 'other property plant & equipment data'.
+   * @return int lastInsertId()
+   */
+  public static function create(
+    $inspection_report_id,
+    $type,
+    $plate_no,
+    $property_no,
+    $engine_no,
+    $chassis_no,
+    $acquisition_date,
+    $acquisition_cost,
+    $repair_history,
+    $repair_date,
+    $nature_of_last_repair,
+    $defects_complaints
+  ) {
+    $sql = 'INSERT INTO motor_vehicles 
+            (inspection_report_id, type, plate_no, property_no, engine_no, chassis_no, acquisition_date, acquisition_cost, repair_history, repair_date, nature_of_last_repair, defects_complaints)
+            VALUES 
+            (:inspection_report_id, :type, :plate_no, :property_no, :engine_no, :chassis_no, :acquisition_date, :acquisition_cost, :repair_history, :repair_date, :nature_of_last_repair, :defects_complaints)';
+
+    return DB::insert($sql, [
+      ':inspection_report_id' => $inspection_report_id,
+      ':type' => $type,
+      ':plate_no' => $plate_no,
+      ':property_no' => $property_no,
+      ':engine_no' => $engine_no,
+      ':chassis_no' => $chassis_no,
+      ':acquisition_date' => $acquisition_date,
+      ':acquisition_cost' => $acquisition_cost,
+      ':repair_history' => $repair_history,
+      ':repair_date' => $repair_date,
+      ':nature_of_last_repair' => $nature_of_last_repair,
+      ':defects_complaints' => $defects_complaints
+    ]);
+  }
+}
