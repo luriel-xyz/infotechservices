94196c659a45a11cb489fbe06567a724772305c5
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-09 13:15:27 +0800

    add label

--- resources/views/includes/forms/personnelUserAccount-addingform.view.php
+++ resources/views/includes/forms/personnelUserAccount-addingform.view.php
@@ -1,7 +1,7 @@
 <form enctype="multipart/form-data" id="personnelUserAccount-form">
 	<div class="modal-header text-dark border-bottom pb-3">
 		<div class="container-fluid text-center">
-			<p class="h5 modal-title" id="exampleModalLabel">PERSONNEL ACCOUNT ADDING FORM</p> 
+			<p class="h5 modal-title" id="exampleModalLabel">PERSONNEL ACCOUNT ADDING FORM</p>
 		</div>
 		<a class="p-1" type="button" data-dismiss="modal" aria-label="Close" onclick="$('#modalPersonnelAccount').modal('hide')">
 			<span aria-hidden="true">&times;</span>
@@ -10,13 +10,14 @@
 
 	<div class="modal-body">
 		<div class="form-group">
-			<input type="hidden" class="form-control action" name="action" id="action" value="addPersonnelUserAccount"> 
+			<input type="hidden" class="form-control action" name="action" id="action" value="addPersonnelUserAccount">
 		</div>
 
 		<div id="useraccount_id" class="useraccount_id">
 		</div>
 
 		<div class="form-group">
+			<label for="usertype" class="font-size-small mb-0 pb-0">Usertype</label>
 			<select name="usertype" id="usertype" class="form-control usertype" required>
 				<option selected disabled> -- Select User Type -- </option>
 				<option value="admin"> Admin </option>
@@ -25,6 +26,7 @@
 		</div>
 
 		<div class="form-group">
+			<label for="emp_id" class="font-size-small mb-0 pb-0">Employee</label>
 			<select name="emp_id" id="emp_id" class="form-control">
 				<option selected disabled> -- Select Employee -- </option>
 				<?php foreach ($personnels as $personnel) : ?>
@@ -36,11 +38,17 @@
 		</div>
 
 		<div class="form-group">
-			<input type="text" class="form-control personnel-username" name="username" id="username" placeholder="Username" required>
+			<div class="md-form">
+				<i class="fas fa-user prefix grey-text"></i>
+				<input type="text" class="form-control personnel-username" name="username" id="username" placeholder="Username" required>
+			</div>
 		</div>
 
 		<div class="form-group">
-			<input type="password" class="form-control password" name="password" id="password" placeholder="Password" required>
+			<div class="md-form">
+				<i class="fas fa-lock prefix grey-text"></i>
+				<input type="password" class="form-control password" name="password" id="password" placeholder="Password" required>
+			</div>
 		</div>
 
 	</div>
