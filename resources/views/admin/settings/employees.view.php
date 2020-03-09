0b5750e897f317fd27e7568243783ad5933ba21e
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-09 13:13:34 +0800

    update

--- resources/views/admin/settings/employees.view.php
+++ resources/views/admin/settings/employees.view.php
@@ -11,7 +11,7 @@
   <div class="h-100 w-100">
 
     <!-- Page Title -->
-    <?php view('includes/page-title'); ?>
+    <?php view('includes/navbar'); ?>
     <!-- /# Page Title -->
 
     <div class="container-fluid h-100 mt-4">
@@ -19,7 +19,7 @@
       <div class="md-form">
         <div class="row">
           <div class="col-md-5">
-            <i class="fas fa-search prefix"></i>
+            <i class="fas fa-search prefix grey-text"></i>
             <input type="text" class="form-control " id="search" placeholder="Search">
           </div>
           <button type="button" class="btn btn-sm btn-primary" id="add-employee" data-toggle="tooltip" title="Add Employee"><i class="fa fa-user-plus" aria-hidden="true"></i></button>
@@ -31,7 +31,7 @@
     <!-- Modal  -->
     <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
-        <div class="modal-content"> 
+        <div class="modal-content">
 
           <!-- Forms -->
           <!--  Add Modal  -->
