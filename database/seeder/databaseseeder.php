df9cda971654bc052b98f39dc80fe94cf3890659
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-09 14:13:43 +0800

    Create database seeder

---
+++ database/seeder/databaseseeder.php
@@ -0,0 +1,16 @@
+<?php
+
+try {
+  // opag employee
+  // Employee::create(8, 1, 'opag employee fname', 'opag employee lname', 'opag employee position');
+
+  // pgo it employee
+  // Employee::create(1, 2, 'pgo it employee fname', 'pgo it employee lname', 'pgo it employee position');
+
+  // User::create('admin', 'password');
+  // User::create('opag', 'password');
+
+  echo 'Database seeding completed successfully.';
+} catch (\Exception $e) {
+  die($e->getMessage());
+}
