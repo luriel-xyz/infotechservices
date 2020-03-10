8bf868fc05f179c0e5640fed44f2856862f64f06
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-10 16:05:11 +0800

    Fix bug

--- resources/views/includes/tables/incomingrepair-tbl.view.php
+++ resources/views/includes/tables/incomingrepair-tbl.view.php
@@ -108,6 +108,7 @@ else :
                   ?>
                 <?php elseif ($repair->status === 'pre-post-repair inspected') : ?>
                   <button type="button" class="dropdown-item done-repair" data-toggle="tooltip" id="<?= $repair->itsrequest_id ?>" data-id="<?= user()->useraccount_id ?>">Done</button>
+                  <button type="button" class="dropdown-item btn-print-assessment" data-toggle="tooltip" data-assessment-report-id="<?= $assessmentReport->repassessreport_id ?>">Print Assessment Report</button>
                   <button type="button" class="dropdown-item btn-print-inspection-report" data-toggle="tooltip" id="<?= $repair->itsrequest_id ?>" data-id="<?= user()->useraccount_id ?>" data-assessment-report-id="<?= $assessmentReport->repassessreport_id ?>">Print Inspection Report</button>
                 <?php elseif ($repair->status === 'done') : ?>
                   <button type="button" class="dropdown-item btn-print-assessment" data-toggle="tooltip" data-assessment-report-id="<?= $assessmentReport->repassessreport_id ?>">Print Assessment Report</button>
