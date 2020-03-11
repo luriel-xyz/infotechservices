f3c931f6e4e0dd8eb6aa05e8edb0ded02472c452
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-11 14:34:51 +0800

    Set modal size to large

--- resources/views/admin/settings/employees.view.php
+++ resources/views/admin/settings/employees.view.php
@@ -30,7 +30,7 @@
 
     <!-- Modal  -->
     <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
 
           <!-- Forms -->
