b32a7df0b76efa7462dd394bed279f7792691389
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-11 15:41:49 +0800

    Add env.php

---
+++ env.php
@@ -0,0 +1,15 @@
+<?php
+// database
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DB_NAME', 'infotechservices');
+
+// User types
+define('ADMIN', 'admin');
+define('PERSONNEL', 'personnel');
+define('PROGRAMMER', 'programmer');
+define('DEPARTMENT', 'department');
+
+// App name
+define('APP_NAME', 'infotechservices');
