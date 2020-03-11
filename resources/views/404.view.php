d06e57ac735b954c911d2be97037ab47fdba75aa
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-11 15:42:04 +0800

    Add 404 view file

---
+++ resources/views/404.view.php
@@ -0,0 +1,7 @@
+<?php view('includes/header'); ?>
+
+<div class="d-flex align-items-center">
+  <h1 class="grey-text text-center font-weight-bold">Page not found.</h1>
+</div>
+
+<?php view('includes/footer'); ?>
