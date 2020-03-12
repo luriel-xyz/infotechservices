a7fe2512986efd2576cfc79dee9811d606adbfc1
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-12 11:35:24 +0800

    Add comments

--- config/init.php
+++ config/init.php
@@ -1,6 +1,5 @@
 <?php
 //start session
-
 session_start();
 
 // Set timezone to Asia Manila
@@ -8,13 +7,18 @@ date_default_timezone_set('Asia/Manila');
 
 // Root url
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+// Views directory path
 define('VIEW', ROOT . '/resources/views');
+// Assets directory path
 define('ASSET', ROOT . '/app/assets');
 
-// Autoload file
+// Require autoload file
 require_once(ROOT . '/vendor/autoload.php');
 
+// Adds user data to session if exists.
 if (isset($_POST['user'])) {
+  // Converts user array to object.
   $userObject = json_decode(json_encode($_POST['user']), FALSE);
+  // Adds user object to session
   session('user', $userObject);
 }
