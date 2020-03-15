e0e3a4baed478e8f2f8f4bf4b014ede9d817a097
luriel-xyz <luriel.mapili@yahoo.com>
2020-03-15 14:33:26 +0800

    Update

--- app/admin/assessment-form.php
+++ app/admin/assessment-form.php
@@ -11,12 +11,17 @@ if (!isUserLoggedIn()) {
   exit;
 }
 
-if (isset($_POST['useraccount_id']) && isset($_POST['itsrequest_id'])) {
-  // $useraccount_id = $_POST['useraccount_id'];
-  $itsrequest_id = $_POST['itsrequest_id'];
-  $dept_id = $_POST['dept_id'];
-  $hwcomponent_id = $_POST['hwcomponent_id'];
+if (
+  !isset($_POST['itsrequest_id'])
+  && !isset($_POST['dept_id'])
+  && !isset($_POST['hwcomponent_id'])
+) {
+  redirectToPreviousPage();
 }
+
+$itsrequest_id = $_POST['itsrequest_id'];
+$dept_id = $_POST['dept_id'];
+$hwcomponent_id = $_POST['hwcomponent_id'];
 
 //get all departments
 $departments = Department::all();
@@ -26,11 +31,6 @@ $hardwareComponents = Hardware::getHardwareComponentsByCategory('main');
 
 //get all sub hardware component
 $subHardwareComponents = Hardware::getHardwareComponentsByCategory('sub');
-
-// echo ($_POST['useraccount_id']);
-// echo ($_POST['itsrequest_id']);
-// echo ($_POST['hwcomponent_id']);
-// die;
 
 $data = compact('itsrequest_id', 'dept_id', 'hwcomponent_id', 'departments', 'hardwareComponents', 'subHardwareComponents');
 view('includes/forms/assessment-form', $data);
