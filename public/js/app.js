c47801b061e92ac26a92b224a34fce177221b019
luriel-xyz <luriel.mapili@yahoo.com>
2020-04-04 16:04:38 +0800

    updated something

--- public/js/app.js
+++ public/js/app.js
@@ -61977,8 +61977,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\InfoTechServices\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\InfoTechServices\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\infotechservices\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\infotechservices\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
